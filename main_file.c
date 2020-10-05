067438646d7e6c2bd03d7db44dde26ebbf4258d4
includeavaneesh <54032418+includeavaneesh@users.noreply.github.com>
2020-10-05 12:18:57 +0530

    Update main_file.c

--- main_file.c
+++ main_file.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#define INFINITY 9999
+#define MAX 10
 
+int G[MAX][MAX],n,startnode,destination;
+void duplicate(int n);
+void locAddress();
+void scanning();
+void returnAddress();
+void dijkstra();
+void push();
+void popDisplay();
+int empty();
+
+int main()
+{
+	scanning();
+	dijkstra();
+
+	return 0;
+}
